c0c17f32260268d43ad8edca64679841e2220e11
Seung Jun, Yeon <49359953+2inza@users.noreply.github.com>
2019-05-15 03:37:05 +0900

    Chess

--- Chess/Chess.py
+++ Chess/Chess.py
@@ -4,14 +4,21 @@ import sys
 pg = pygame
 pg.init()
 # =========================================================
-
 # interface
-
 # =========================================================
-# interface
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-SIZE = [700, 500]
+SIZE = [800, 566]
+
+fps = pg.time.Clock()
+Exit = False
+preGame = True
+PickWhite = True
+PieceSelect = False
+temp = 0
+x, y = 0, 0
+oldX, oldY, = 0, 0
 
 board = pg.image.load("sources/board.jpg")
 
@@ -40,130 +47,189 @@ me = text1.render("made by Yeon", True, BLACK)
 button1 = text1.render("White", True, BLACK)
 button2 = text1.render("Black", True, BLACK)
 button3 = text1.render("Reset", True, BLACK)
+
 # =========================================================
 
-fps = pg.time.Clock()
-Exit = False
-preGame = False
-PickWhite = True
-Moving = (0, 0)
 while not Exit:
-    # fps
-    fps.tick(15)
-
-    # ===============================================
-    # first screen.
+    # fps, Mouse info
+    fps.tick(30)
+    MousePos = pg.mouse.get_pos()
+    MouseClick = pg.mouse.get_pressed()
+    # =====================================================================================
+    # pregame
     scr.fill(WHITE)
-    scr.blit(board, (0, 0))
-    scr.blit(me, (600, 480))
-    scr.blit(textSurfaceObj, (565, 10))
-    # ===============================================
+    scr.blit(board, (3, 3))
+    scr.blit(me, (700, 545))
+    scr.blit(textSurfaceObj, (655, 10))
 
+    act = False
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
             Exit = True
             pg.quit()
             sys.exit()
+        elif event.type == pg.MOUSEBUTTONUP:
+            act = True
 
-    MousePos = pg.mouse.get_pos()
-    MouseClick = pg.mouse.get_pressed()
-    if not preGame:
-        pg.draw.rect(scr, BLACK, [500, 40, 90, 30], 1)
-        pg.draw.rect(scr, BLACK, [600, 40, 90, 30], 1)
-        scr.blit(text1Obj, (535, 80))
-        scr.blit(button1, (525, 45))
-        scr.blit(button2, (625, 45))
-        if 500 < MousePos[0] < 590 and 40 < MousePos[1] < 70:
-            pg.draw.rect(scr, BLACK, [500, 40, 90, 30], 5)
-            if MouseClick[0]:
-                preGame = True
-        elif 600 < MousePos[0] < 690 and 40 < MousePos[1] < 70:
-            pg.draw.rect(scr, BLACK, [600, 40, 90, 30], 5)
-            if MouseClick[0]:
-                preGame = True
-                PickWhite = False
+    if MouseClick[0]:
+        if 3 < MousePos[0] < 73: x = 0
+        elif MousePos[0] < 143: x = 1
+        elif MousePos[0] < 213: x = 2
+        elif MousePos[0] < 283: x = 3
+        elif MousePos[0] < 353: x = 4
+        elif MousePos[0] < 423: x = 5
+        elif MousePos[0] < 493: x = 6
+        elif MousePos[0] < 563: x = 7
+        else: x = "None"
+
+        if 3 < MousePos[1] < 73: y = 0
+        elif MousePos[1] < 143: y = 1
+        elif MousePos[1] < 213: y = 2
+        elif MousePos[1] < 283: y = 3
+        elif MousePos[1] < 353: y = 4
+        elif MousePos[1] < 423: y = 5
+        elif MousePos[1] < 493: y = 6
+        elif MousePos[1] < 563: y = 7
+        else: y = "None"
 
     if preGame:
-        pg.draw.rect(scr, BLACK, [500, 465, 90, 30], 1)
-        scr.blit(button3, (527, 470))
-        if 500 < MousePos[0] < 590 and 465 < MousePos[1] < 495:
-            pg.draw.rect(scr, BLACK, [500, 465, 90, 30], 5)
+        pg.draw.rect(scr, BLACK, [588, 40, 90, 30], 1)
+        pg.draw.rect(scr, BLACK, [688, 40, 90, 30], 1)
+        scr.blit(text1Obj, (620, 80))
+        scr.blit(button1, (613, 45))
+        scr.blit(button2, (714, 45))
+        if 588 < MousePos[0] < 678 and 40 < MousePos[1] < 70:
+            pg.draw.rect(scr, BLACK, [588, 40, 90, 30], 5)
             if MouseClick[0]:
                 preGame = False
+        elif 688 < MousePos[0] < 778 and 40 < MousePos[1] < 70:
+            pg.draw.rect(scr, BLACK, [688, 40, 90, 30], 5)
+            if MouseClick[0]:
+                preGame = False
+                PickWhite = False
+
+        BoardArray = [[[5, 3, 2, 8, 9, 2, 3, 5],
+                       [1, 1, 1, 1, 1, 1, 1, 1],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [-1, -1, -1, -1, -1, -1, -1, -1],
+                       [-5, -3, -2, -8, -9, -2, -3, -5]],
+
+                      [[-5, -3, -2, -9, -8, -2, -3, -5],
+                       [-1, -1, -1, -1, -1, -1, -1, -1],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 1, 1, 1, 1, 1],
+                       [5, 3, 2, 9, 8, 2, 3, 5]]]
+
+        PieceSelect = False
+        temp = 0
+        oldX, oldY, = 0, 0
+        Turn = 1
+    # =====================================================================================
+    if not preGame:
+        pg.draw.rect(scr, BLACK, [588, 532, 90, 30], 1)
+        scr.blit(button3, (615, 537))
+        if 590 < MousePos[0] < 680 and 532 < MousePos[1] < 562:
+            pg.draw.rect(scr, BLACK, [590, 532, 90, 30], 5)
+            if MouseClick[0]:
+                preGame = True
                 PickWhite = True
-        if PickWhite:
-            scr.blit(bRook, (13 + Moving[0], 11 + Moving[1]))
-            scr.blit(bKnight, (73 + Moving[0], 11 + Moving[1]))
-            scr.blit(bBishop, (136 + Moving[0], 11 + Moving[1]))
-            scr.blit(bQueen, (195 + Moving[0], 11 + Moving[1]))
-            scr.blit(bKing, (255 + Moving[0], 11 + Moving[1]))
-            scr.blit(bBishop, (315 + Moving[0], 11 + Moving[1]))
-            scr.blit(bKnight, (375 + Moving[0], 11 + Moving[1]))
-            scr.blit(bRook, (435 + Moving[0], 11 + Moving[1]))
 
-            scr.blit(wRook, (13 + Moving[0], 433 + Moving[1]))
-            scr.blit(wKnight, (73 + Moving[0],433 + Moving[1]))
-            scr.blit(wBishop, (136 + Moving[0], 433 + Moving[1]))
-            scr.blit(wQueen, (195 + Moving[0], 433 + Moving[1]))
-            scr.blit(wKing, (255 + Moving[0], 433 + Moving[1]))
-            scr.blit(wBishop, (315 + Moving[0], 433 + Moving[1]))
-            scr.blit(wKnight, (375 + Moving[0], 433 + Moving[1]))
-            scr.blit(wRook, (435 + Moving[0], 433 + Moving[1]))
+        if Turn % 2 == 1:
+            pg.draw.rect(scr, BLACK, [588, 40, 90, 30], 1)
+            scr.blit(button1, (613, 45))
+        else:
+            pg.draw.rect(scr, BLACK, [688, 40, 90, 30], 1)
+            scr.blit(button2, (714, 45))
 
-            scr.blit(bPawn, (11 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (72 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (133 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (194 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (255 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (316 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (377 + Moving[0], 71 + Moving[1]))
-            scr.blit(bPawn, (438 + Moving[0], 71 + Moving[1]))
+        for h in range(8):
+            for w in range(8):
+                if BoardArray[PickWhite][h][w] < 0:
+                    if BoardArray[PickWhite][h][w] * -1 == 5:
+                        scr.blit(bRook, ((w * 70) + 1, (h * 70) + 2))
+                    elif BoardArray[PickWhite][h][w] * -1 == 3:
+                        scr.blit(bKnight, ((w * 70) + 1, (h * 70) + 2))
+                    elif BoardArray[PickWhite][h][w] * -1 == 2:
+                        scr.blit(bBishop, ((w * 70) + 1, (h * 70) + 2))
+                    elif BoardArray[PickWhite][h][w] * -1 == 9:
+                        scr.blit(bQueen, ((w * 70) + 1, (h * 70) + 2))
+                    elif BoardArray[PickWhite][h][w] * -1 == 8:
+                        scr.blit(bKing, ((w * 70) + 1, (h * 70) + 2))
+                    elif BoardArray[PickWhite][h][w] * -1 == 1:
+                        scr.blit(bPawn, ((w * 70) + 1, (h * 70) + 2))
+                else:
+                    if BoardArray[PickWhite][h][w] == 5:
+                        scr.blit(wRook, ((w * 70) + 1, (h * 70) + 3))
+                    elif BoardArray[PickWhite][h][w] == 3:
+                        scr.blit(wKnight, ((w * 70) + 1, (h * 70) + 3))
+                    elif BoardArray[PickWhite][h][w] == 2:
+                        scr.blit(wBishop, ((w * 70) + 1, (h * 70) + 3))
+                    elif BoardArray[PickWhite][h][w] == 9:
+                        scr.blit(wQueen, ((w * 70) + 1, (h * 70) + 3))
+                    elif BoardArray[PickWhite][h][w] == 8:
+                        scr.blit(wKing, ((w * 70) + 1, (h * 70) + 3))
+                    elif BoardArray[PickWhite][h][w] == 1:
+                        scr.blit(wPawn, ((w * 70) + 1, (h * 70) + 3))
 
-            scr.blit(wPawn, (11 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (72 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (133 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (194 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (255 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (316 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (377 + Moving[0], 373 + Moving[1]))
-            scr.blit(wPawn, (438 + Moving[0], 373 + Moving[1]))
-
-        if not PickWhite:
-            scr.blit(wRook, (13 + Moving[0], 11 + Moving[1]))
-            scr.blit(wKnight, (73 + Moving[0], 11 + Moving[1]))
-            scr.blit(wBishop, (136 + Moving[0], 11 + Moving[1]))
-            scr.blit(wKing, (195 + Moving[0], 11 + Moving[1]))
-            scr.blit(wQueen, (255 + Moving[0], 11 + Moving[1]))
-            scr.blit(wBishop, (315 + Moving[0], 11 + Moving[1]))
-            scr.blit(wKnight, (375 + Moving[0], 11 + Moving[1]))
-            scr.blit(wRook, (435 + Moving[0], 11 + Moving[1]))
-
-            scr.blit(bRook, (13 + Moving[0], 433 + Moving[1]))
-            scr.blit(bKnight, (73 + Moving[0], 433 + Moving[1]))
-            scr.blit(bBishop, (136 + Moving[0], 433 + Moving[1]))
-            scr.blit(bKing, (195 + Moving[0], 433 + Moving[1]))
-            scr.blit(bQueen, (255 + Moving[0], 433 + Moving[1]))
-            scr.blit(bBishop, (315 + Moving[0], 433 + Moving[1]))
-            scr.blit(bKnight, (375 + Moving[0], 433 + Moving[1]))
-            scr.blit(bRook, (435 + Moving[0], 433 + Moving[1]))
-
-            scr.blit(wPawn, (11 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (72 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (133 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (194 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (255 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (316 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (377 + Moving[0], 71 + Moving[1]))
-            scr.blit(wPawn, (438 + Moving[0], 71 + Moving[1]))
-
-            scr.blit(bPawn, (11 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (72 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (133 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (194 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (255 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (316 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (377 + Moving[0], 373 + Moving[1]))
-            scr.blit(bPawn, (438 + Moving[0], 373 + Moving[1]))
+        if MouseClick[0] and act and x != "None" and y != "None":
+            if Turn % 2 == 1:
+                if BoardArray[PickWhite][y][x] != 0 and PieceSelect:
+                    if (temp > 0) == (BoardArray[PickWhite][y][x] > 0):
+                        PieceSelect = False
+                        temp = 0
+                        print("select cancel")
+                    else:
+                        BoardArray[PickWhite][y][x] = temp
+                        BoardArray[PickWhite][oldY][oldX] = 0
+                        oldX, oldY = 0, 0
+                        temp = 0
+                        PieceSelect = False
+                        Turn += 1
+                        print("get piece")
+                elif 0 < BoardArray[PickWhite][y][x] != 0 and not PieceSelect:
+                    temp = BoardArray[PickWhite][y][x]
+                    oldX, oldY = x, y
+                    PieceSelect = True
+                    print("select")
+                elif BoardArray[PickWhite][y][x] == 0 and PieceSelect:
+                    BoardArray[PickWhite][oldY][oldX] = 0
+                    oldX, oldY = 0, 0
+                    BoardArray[PickWhite][y][x] = temp
+                    temp = 0
+                    PieceSelect = False
+                    Turn += 1
+                    print("move to empty")
+            elif Turn % 2 == 0:
+                if BoardArray[PickWhite][y][x] != 0 and PieceSelect:
+                    if (temp < 0) == (BoardArray[PickWhite][y][x] < 0):
+                        PieceSelect = False
+                        temp = 0
+                        print("select cancel")
+                    else:
+                        BoardArray[PickWhite][y][x] = temp
+                        BoardArray[PickWhite][oldY][oldX] = 0
+                        temp = 0
+                        PieceSelect = False
+                        Turn += 1
+                        print("get piece")
+                elif 0 > BoardArray[PickWhite][y][x] != 0 and not PieceSelect:
+                    temp = BoardArray[PickWhite][y][x]
+                    oldX, oldY = x, y
+                    PieceSelect = True
+                    print("select")
+                elif BoardArray[PickWhite][y][x] == 0 and PieceSelect:
+                    BoardArray[PickWhite][oldY][oldX] = 0
+                    oldX, oldY = 0, 0
+                    BoardArray[PickWhite][y][x] = temp
+                    temp = 0
+                    PieceSelect = False
+                    Turn += 1
+                    print("move to empty")
     pg.display.flip()
 
 
